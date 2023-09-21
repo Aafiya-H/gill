d768fc753f04ef1bf39e846f9c59ebba04325695
Aafiya-H <aafiya.hussain2@gmail.com>
2023-09-21 11:52:08 +0530

    refactored audiocaps download code

--- scripts/download_audiocaps_csv.sh
+++ scripts/download_audiocaps_csv.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # URL of the CSV file on GitHub
-url="https://raw.githubusercontent.com/cdjkim/audiocaps/master/dataset/train.csv"
+split="$1" # ["train","val","test"]
+url="https://raw.githubusercontent.com/cdjkim/audiocaps/master/dataset/$split.csv"
 
 # Local file path to save the downloaded file
-local_file_path="../datasets/AudioCaps/train.csv"
+local_file_path="../datasets/AudioCaps/$split.csv"
 
 # Use wget to download the CSV file
 wget -O "$local_file_path" "$url"
--- scripts/download_audioclips.py
+++ scripts/download_audioclips.py
@@ -2,17 +2,29 @@ from audiocaps_download import Downloader
 from tqdm import tqdm
 import pandas as pd
 import os
+import argparse
 
-split = "test"
-# print(os.getcwd())
-# os.system("ls")
-d = Downloader(root_path='datasets/AudioCaps', n_jobs=10)
-df = pd.read_csv(f"datasets/AudioCaps/{split}.csv")
+def download_audiocaps(split):
+    df = pd.read_csv(f"datasets/AudioCaps/{split}.csv")
 
-root_path = f"datasets/AudioCaps/{split}"
+    root_path = f"datasets/AudioCaps/{split}"
+    if not os.path.exists(root_path):
+        os.mkdir(root_path)
 
-for index,row in tqdm(df.iterrows(),total=len(df)):
-    target_file_path = os.path.join(root_path,str(row["audiocap_id"]) + ".wav")
-    start_seconds = row["start_time"]
-    yt_id = row["youtube_id"]
-    os.system(f'yt-dlp -x --audio-format wav --audio-quality 5 --output "{target_file_path}" --postprocessor-args "-ss {start_seconds} -to {start_seconds+10}" https://www.youtube.com/watch?v={yt_id}')
+    for index, row in tqdm(df.iterrows(), total=len(df)):
+        target_file_path = os.path.join(root_path, str(row["audiocap_id"]) + ".wav")
+        start_seconds = row["start_time"]
+        yt_id = row["youtube_id"]
+        os.system(f'yt-dlp -x --audio-format wav --audio-quality 5 --output "{target_file_path}" --postprocessor-args "-ss {start_seconds} -to {start_seconds+10}" https://www.youtube.com/watch?v={yt_id}')
+
+def main():
+    parser = argparse.ArgumentParser(description="Download AudioCaps data for a specific split.")
+    parser.add_argument("--split", type=str, help="Specify the split to download (e.g., 'train', 'val', 'test').")
+
+    args = parser.parse_args()
+    split = args.split
+
+    download_audiocaps(split)
+
+if __name__ == "__main__":
+    main()
